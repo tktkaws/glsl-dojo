e3d40f5b9622d01ac14e0a4cd428cb8622cb3a05
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-09-10 14:16:02 +0900

    Fix: buildオプション

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import glsl from "vite-plugin-glsl";
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [glsl()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "other/index.html"),
+      },
+    },
+  },
 });
